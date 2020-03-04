c696911deee5de2d22d1086f855dc95bdcfc608b
忧里修斯 <769435570@qq.com>
2020-03-04 22:03:20 +0800

    有序数组

--- array.py
+++ array.py
@@ -29,26 +29,54 @@ class Array(object):
                 
     def printf(self):
         print(self.data)
+
+
+class SortedArray(object):
+    """大小固定的有序数组（升序），支持动态增删操作"""
+    def __init__(self, capacity=10):
+        self.capacity = capacity
+        self.size = 0
+        self.data = [None] * capacity
+
+    def add(self, value):
+        if self.size == self.capacity:
+            print('数组已满')
+        else:
+            for i, v in enumerate(self.data):
+                if v is None:
+                    self.data[i] = value
+                    self.size += 1
+                    break
+                else:
+                    if value <= v:    # 确定了插入的位置
+                        for x in range(self.size, i, -1):    # 元素后移
+                            self.data[x] = self.data[x-1]
+                        self.data[i] = value
+                        self.size += 1
+                        break
+                    else:
+                        continue
         
-    
+
+    def remove(self, value):
+        for i, v in enumerate(self.data):
+            if v == value:    # 找到元素位置
+                self.data[i] = None
+                for x in range(i + 1, self.size):    # 元素后移
+                    self.data[x] = self.data[x+1]
+                self.size -= 1
+
+        pass
+        
+
+def test_SortedArray():
+    tmp = [9, 1, 2, 7, 8, 6, 5, 10, 4, 3]
+    # tmp = [9, 1, 2, 7]
+    sortedArray = SortedArray()
+    for v in tmp:
+        sortedArray.add(v)
+        print('size:', sortedArray.size)
+        print(sortedArray.data)
 
 if __name__ == '__main__':
-    a = Array()
-    a.printf()
-    a.add(1)
-    a.add(2)
-    a.add(3)
-    a.add(4)
-    a.add(5)
-    a.add(6)
-    a.add(7)
-    a.add(8)
-    a.add(9)
-    a.add(10)
-    a.printf()
-    a.add(11)
-    a.printf()
-    a.add(12)
-    a.printf()
-    a.remove(7)
-    a.printf()
+    test_SortedArray()
